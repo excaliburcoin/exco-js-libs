56216c345deaad719e24f83e4e845219c68b411d
excaliburcoin <saradaexcalibur@gmail.com>
2018-12-27 20:01:19 +0900

    fix a bug in kakuyomu-notation

--- packages/kakuyomu-notation/index.js
+++ packages/kakuyomu-notation/index.js
@@ -3,7 +3,7 @@ const RUBY_REGEX = /[｜|]([^\n]{1,50}?)《([^\n]{1,20}?)》/g
 const KANJI_RUBY_REGEX = /([\u2e80-\u2fdf\u3005\3007\303b\u4e00-\u9faf\u3400-\u4dbf\uf900-\ufaff]{1,50}?)《([^\n]{1,20}?)》/g
 
 module.exports = function transformKakuyomuNotation (text) {
-  text = text.replace(DOT_REGEX1, function (match, p1){
+  text = text.replace(DOT_REGEX, function (match, p1){
     const text = [...p1].map(a => `<span>${a}</span>`).join('')
     return `<em class="emphasis">${text}</em>`
   })
